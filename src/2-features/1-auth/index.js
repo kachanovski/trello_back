512ee96b4ecc05ca974a9de3a00ba9636d4c1cae
kachanovski03@gmail.com <13shurik03>
2021-03-17 02:31:35 +0300

    test cors

--- src/2-features/1-auth/index.js
+++ src/2-features/1-auth/index.js
@@ -11,7 +11,7 @@ let auth = express.Router();
 
 let corsOptions = {
     credentials: true,
-    origin: 'http://localhost:3000'
+    origin: 'http://localhost:3000/'
 }
 
 auth.post('/login',cors(corsOptions), login)
