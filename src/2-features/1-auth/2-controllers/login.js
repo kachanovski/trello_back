9bb7d9ed170dd8f571ebb0299dde4298af6bdfb2
kachanovski03@gmail.com <13shurik03>
2021-03-19 23:35:37 +0300

    fix auth

--- src/2-features/1-auth/2-controllers/login.js
+++ src/2-features/1-auth/2-controllers/login.js
@@ -7,64 +7,62 @@ const {config} = require('../../../1-main/config')
 const login = async (req, res) => {
 
     const {email, password, rememberMe} = req.body;
-    if (validateAuth(req, res)) {
-        try {
-            const user = await User.findOne({email}).exec()
-            if (!user) {
-                res.status(400).json({
-                    resultCode: 1,
-                    message: 'User not found'
-                })
-            } else if (!(await bCrypt.compare(password, user.password))) {
-                res.status(400).json({
-                    resultCode: 1,
-                    message: 'Incorrect password'
-                })
-            } else {
-
-                const token = jwt.sign(
-                    {
-                        id: user._id,
-                        email: user.email
-                    },
-                    config.jwtSecret,
-                    {expiresIn: '24h'}
-                )
-
-                try {
-                    const newUser = await User.findByIdAndUpdate(
-                        user._id,
-                    ).exec()
-                    if (!newUser) {
-                        res.status(500).json({
-                            resultCode: 1,
-                            message: "login/user maybe toten?"
-                        })
-                    } else {
-                        res.cookie('token', token)
-                            .status(201).json({
-                            data: {
-                                id: user._id,
-                                email: user.email,
-                                rememberMe: user.rememberMe,
-                            },
-                            resultCode: 0
-                        });
-                    }
-
-                } catch (e) {
-                    res.status(400).json({
-                        resultCode: 1,
-                        message: 'Yoops, something went wrong(login)'
-                    })
-                }
-            }
-        } catch (e) {
-            res.status(500).json({
+    try {
+        const user = await User.findOne({email}).exec()
+        if (!user) {
+            res.status(400).json({
                 resultCode: 1,
-                message: 'Yoops, something went wrong.'
+                message: 'User not found'
             })
+        } else if (!(await bCrypt.compare(password, user.password))) {
+            res.status(400).json({
+                resultCode: 1,
+                message: 'Incorrect password'
+            })
+        } else {
+
+            const token = jwt.sign(
+                {
+                    id: user._id,
+                    email: user.email
+                },
+                config.jwtSecret,
+                {expiresIn: '24h'}
+            )
+
+            try {
+                const newUser = await User.findByIdAndUpdate(
+                    user._id,
+                ).exec()
+                if (!newUser) {
+                    res.status(500).json({
+                        resultCode: 1,
+                        message: "login/user maybe toten?"
+                    })
+                } else {
+                    res.cookie('token', token)
+                        .status(201).json({
+                        data: {
+                            id: user._id,
+                            email: user.email,
+                            rememberMe: user.rememberMe,
+                        },
+                        resultCode: 0
+                    });
+                }
+
+            } catch (e) {
+                res.status(400).json({
+                    resultCode: 1,
+                    message: 'Yoops, something went wrong(login)'
+                })
+            }
         }
+    } catch (e) {
+        res.status(500).json({
+            resultCode: 1,
+            message: 'Yoops, something went wrong.'
+        })
     }
 }
 
--- src/2-features/1-auth/2-controllers/register.js
+++ src/2-features/1-auth/2-controllers/register.js
@@ -6,47 +6,45 @@ const register = async (req, res) => {
     console.log(req.body)
     const {email, password} = req.body
 
-    if (validateAuth(req, res)) {
-        try {
-            const oldUser = await User.findOne({email}).exec()
-            if (oldUser) {
-                res.status(400).json({
-                    resultCode: 1,
-                    message: 'email exists'
-                })
-            } else {
-                const user = await User.create(
-                    {
-                        email,
-                        password: await bCrypt.hash(password, 8),
-                        rememberMe: false,
-
-                        userName: email,
-                        verified: false,
-
-                        teams: [],
-                        boards: []
-                    }
-                )
-
-                const addedUser = {...user._doc};
-
-                delete addedUser.password; // ne visilai parol` na front!!!!!
-
-                res.status(201).json({
-                    resultCode: 0,
-                    message: 'User created'
-                })
-            }
-        } catch (e) {
-            console.log(e)
-            res.status(500).json({
+    try {
+        console.log('1')
+        const oldUser = await User.findOne({email}).exec()
+        if (oldUser) {
+            res.status(400).json({
                 resultCode: 1,
-                message: 'Yoops, something went wrong.'
+                message: 'email exists'
+            })
+        } else {
+            const user = await User.create(
+                {
+                    email,
+                    password: await bCrypt.hash(password, 8),
+                    rememberMe: false,
+
+                    userName: email,
+                    verified: false,
+
+                    teams: [],
+                    boards: []
+                }
+            )
+
+            const addedUser = {...user._doc};
+
+            delete addedUser.password; // ne visilai parol` na front!!!!!
+
+            res.status(201).json({
+                resultCode: 0,
+                message: 'User created'
             })
         }
+    } catch (e) {
+        console.log(e)
+        res.status(500).json({
+            resultCode: 1,
+            message: 'Yoops, something went wrong.'
+        })
     }
-
 }
 
 module.exports = register
